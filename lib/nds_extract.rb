5296c38e48d56ffdb2b78e47340bd5be39bd46e6
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-13 20:48:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
+require 'pp'
 
 def directors_totals(source)
   result = {}
@@ -26,6 +27,7 @@ end
 
 def list_of_directors(source)
   # Write this implementation
+
 end
 
 def total_gross(source)
@@ -39,5 +41,3 @@ def total_gross(source)
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
 end
-
-
